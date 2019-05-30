f1c353f6674ac0462244812a21ba73c6f028b91f
Quentin Quaadgras <email@quentinquaadgras.com>
2019-05-30 22:38:35 +1200

    Allow custom directory

--- reqgen.py
+++ reqgen.py
@@ -2,21 +2,30 @@
 
 import os
 import pathlib
+import sys
+
+directory = "assets"
 
 def reqgen(dir):
 	files = os.listdir(dir)
 
-	if dir == "assets":
+	if dir == directory:
 		dir = "./"
-		os.chdir("assets")
+		os.chdir(directory)
 
 	for file in files:
 		if os.path.isdir(dir+file):
 			reqgen(dir+file+"/")
 		else:
 			if pathlib.Path(file).suffix == ".png":
-				print("\tcase (\""+dir+file+"\"): return require(\"./assets/"+dir+file+"\");")
+				print("\tcase (\""+dir+file+"\"): return require(\"./"+directory+"/"+dir+file+"\");")
+
+
+
+
+if len(sys.argv) > 1:
+	directory = sys.argv[1]
 
 print("export function GetImage(name) {\nswitch(name) {")
-reqgen("assets")
+reqgen(directory)
 print("}\n}")
